10585b3c6d5e32d36ebf70c41d67085c888c84ed
apple <apple@apples-MacBook-Pro.local>
2021-05-01 11:21:17 +0530

    Added skeleton code of springboot microservice

---
+++ innomart/src/main/java/com/innovationstack/innomart/ComInnomartApplication.java
@@ -0,0 +1,13 @@
+package com.innovationstack.innomart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComInnomartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComInnomartApplication.class, args);
+	}
+
+}
---
+++ innomart/src/test/java/com/innovationstack/innomart/ComInnomartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovationstack.innomart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComInnomartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
