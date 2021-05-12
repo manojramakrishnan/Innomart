2603f8968cbf77d66c5b331e3ce62e27a7f0e562
Manoj Ramakrishnan <manoj.rgv@gmail.com>
2021-05-12 15:24:34 +0530

    added email functionality

--- innomart/src/main/java/com/innovationstack/innomart/configs/AppConfig.java
+++ innomart/src/main/java/com/innovationstack/innomart/configs/AppConfig.java
@@ -1,10 +1,13 @@
 package com.innovationstack.innomart.configs;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
 
 @Configuration
 @PropertySource("classpath:/application.properties")
@@ -15,5 +18,13 @@ public class AppConfig {
     public String getValueOfKey(String key) {
         return env.getProperty(key);
     }
+    
+    @Primary
+	@Bean 
+	public FreeMarkerConfigurationFactoryBean factoryBean() {
+		FreeMarkerConfigurationFactoryBean bean=new FreeMarkerConfigurationFactoryBean();
+		bean.setTemplateLoaderPath("classpath:/templates");
+		return bean;
+	}
 
 }
